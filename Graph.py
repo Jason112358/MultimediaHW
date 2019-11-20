03279c8e006350d09b54acb6c314a8f8835674c9
Jason <jason112358@163.com>
2019-11-20 15:50:00 +0800

    2019-11-20

--- Graph.py
+++ Graph.py
@@ -8,32 +8,35 @@ import numpy as np
 import networkx as nx 
 import matplotlib.pyplot as plt
 
+#打开文件
 N = open("X:\\0931Xu\\Study\\多媒体技术\\Homework\\第四次作业\\structure.col")
-i = 0
-matrix = np.zeros((501,501),dtype = 'int16')    # 创建500*500全零二维数组
-Colors = np.zeros(501,dtype = 'int16')  # 创建规模500的全零一维数组
-G = nx.Graph()
 
+i = 0
+matrix = np.zeros((501,501),dtype = 'int16')    # 创建500*500全零二维数组存储结点的邻接矩阵
+Colors = np.zeros(501,dtype = 'int16')  # 创建规模500的全零一维数组用于存储颜色信息
+G = nx.Graph()
+#以行为单位循环遍历文件
 for line in N:
-    if line[0] in ['E','e']:
+    if line[0] in ['E','e']:#如果是边的信息则初始化两个节点变量
         node1=""
         node2=""
         r = 2
-        while line[r] not in [' ']:
-            node1 += line[r]
-            r = r+1
+        while line[r] not in [' ']:#以空格判断
+            node1 += line[r]#将节点标号传入字符串
+            r = r+1#字符串数加一
         r = r+1
         while line[r] not in [' ','\n']:
-            node2 += line[r]
+            node2 += line[r]#将节点标号传入字符串
             r = r+1
         Node1 = int(node1)  #传入结点1
         Node2 = int(node2)  #传入结点2
-        matrix[Node1][Node2] = 1
-        G.add_edge(Node1,Node2)
+        matrix[Node1][Node2] = 1 #标记两节点间有连线
+        G.add_edge(Node1,Node2) #在图中添加边
 #        print(line[0:-1])
 N.close()
-pos = nx.spring_layout(G)
+pos = nx.spring_layout(G)#spring分布
 """
+输出检查
 for m in range(1,50):
     for n in range(1,5):
         print(matrix[m][n])
